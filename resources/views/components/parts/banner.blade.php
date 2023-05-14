687bb6a729dcc1cf0afe8186ed4411aecd2a1cf7
budimawan <budidharmawanp@gmail.com>
2023-05-14 23:04:54 +0800

    melakukan perbaikan tampilan

---
+++ resources/views/components/parts/banner.blade.php
@@ -0,0 +1,134 @@
+<div class=""
+    style="background: url('https://www.qwords.com/wp-content/themes/qwords/assets/images/shapes/webp/main-bg.webp')">
+    <div class="container-md pt-5">
+        <!-- <div class="d-flex mt-5">
+            <div class="mx-1 flex-grow-1">
+                <input type="text" class="form-control form-control-md mx-1 my-1" placeholder="Cari Nama Domain Anda"
+                    name="">
+            </div>
+            <div class="mx-1 my-1 ">
+                <select class="form-control-md form-control">
+                    <option>.id</option>
+                    <option>.com</option>
+                    <option>.net</option>
+                </select>
+            </div>
+            <div class="">
+                <button class="btn px-3 mx-1 my-1 btn-primary rounded-pill">Cek Domain</button>
+            </div>
+        </div> -->
+        <div class="d-flex">
+            <div class="px-2 py-5 col-7">
+                <p class="fs-1 mt-3 fw-bold">
+                    Cloud Hosting <br> untuk <strong class="text-primary"> Kesuksesan Websitemu!</strong>
+                </p>
+                <p class="fw-light">
+                    Qwords menawarkan layanan Cloud Hosting yang Cepat, Aman, dan Dukungan 24 jam untuk memenuhi
+                    kebutuhan hosting Anda. Tersertifikasi ISO 27001 dengan Data Center Tier 3.
+                </p>
+                <p class="mt-5 mb-0 text-primary">
+                    <strong>Jangan Lewatkan!</strong>
+                </p>
+                <p class="my-0 fs-6 fw-bold">
+                    Cek sekarang dan dapatkan promo spesial hingga <span class="text-danger">50%!</span>
+                </p>
+                <div class="d-flex mt-0">
+                    <!-- <div class="d-flex mt-5"> -->
+                    <div class="mx-0 flex-grow-1">
+                        <input type="text" class="form-control form-control-md mx-1 my-1"
+                            placeholder="Cari Nama Domain Anda" name="">
+                    </div>
+                    <div class="mx-2 my-1 ">
+                        <select class="form-control-md form-control">
+                            <option>.id</option>
+                            <option>.com</option>
+                            <option>.net</option>
+                        </select>
+                    </div>
+                    <div class="">
+                        <button class="btn px-3 mx-1 my-1 btn-primary rounded-pill">Cek Domain</button>
+                    </div>
+                    <!-- </div> -->
+                    <!-- <button class="btn px-5 mx-1 my-3 btn-primary rounded-pill">Cek Domain</button>
+                    <button class="btn px-5 mx-1 my-3 btn-dark rounded-pill"> 🎁 Lihat Promo</button> -->
+                </div>
+
+                <p class="fs-7 mt-5">
+                    Terdaftar di PSE, ICANN dan Partner Lainnya
+                </p>
+                <div class=" mx-auto w-100">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/code.webp">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/icann-logo.webp">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/logobsi-black.webp">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/badge_whmadmin.svg">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/acronis-cloud-tech.webp">
+                    <img style="max-height: 64px" class="mx-1"
+                        src="https://www.qwords.com/wp-content/themes/qwords/assets/images/icons/acronis-cloud-sales.webp">
+                </div>
+            </div>
+            <div class="col-1"></div>
+            <div class="p-5 w-50 mt-5 rounded-pill">
+                <div class="border border-light bg-light rounded shadow mx-1 mt-n3"
+                    style="background: url('https://www.qwords.com/wp-content/themes/qwords/assets/images/shapes/webp/main-bg.webp')">
+                    <div
+                        class="bg-info text-white fw-bold rounded-top text-center p-2 fs-8 animate__animated animate__flash">
+                        Best Offers!
+                    </div>
+                    <div class="px-3">
+                        <div class="text-center py-3  px-3">
+                            <p class="my-0 text-dark fw-semibold small">
+                                <small class="text-secondary">(HPCH Bisnis 1)</small><br>
+                                High Performance
+                            </p>
+                            <p class="">
+                                <strong class="text-primary fs-4 fw-bolder">
+                                    Rp 79.000 <small class="fs-6">/ Bulan</small>
+                                </strong> <br>
+                                <small>
+                                    <s class="text-secondary">Rp 90.000</s>
+                                </small>
+                            </p>
+                        </div>
+                        <div class="">
+                            <div class="d-flex">
+                                <i class='bx bx-xs bx-hdd text-primary'></i>
+                                <p class="ms-2 small">
+                                    Disk <strong>3 GB</strong>
+                                </p>
+                            </div>
+
+                            <div class="d-flex">
+                                <i class='bx bx-xs bx-wifi text-primary'></i>
+                                <p class="ms-2 small">
+                                    Bandwith <strong>Unlimited</strong>
+                                </p>
+                            </div>
+
+                            <div class="d-flex">
+                                <i class='bx bx-xs bx-bolt-circle text-primary'></i>
+                                <p class="ms-2 small">
+                                    CPU <strong>1 Core</strong>
+                                </p>
+                            </div>
+
+                            <div class="d-flex">
+                                <i class='bx bx-xs bx-check-shield text-primary'></i>
+                                <p class="ms-2 small">
+                                    <strong>No</strong> Addon Domain
+                                </p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="bg-primary text-white fw-bold rounded-bottom text-center p-2 fs-8">
+                        <a href="" class="btn text-white fw-bold">Pesan Sekarang</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ resources/views/components/parts/solution.blade.php
@@ -0,0 +1,121 @@
+<div class="pb-5 pt-3">
+    <div class="container-lg">
+        <div class="py-5 mx-auto text-center px-2">
+            <p class="fs-3 mb-0 mt-3 fw-bold">
+                Solusi <strong class="text-primary"> Paket Hosting Terbaik </strong> dari Kami
+            </p>
+            <p class=" fw-normal">
+                Kami menawarkan solusi paket hosting terbaik untuk segala kebutuhan <br>website dan sistem Anda. Mulai
+                dari blog hingga website bisnis dan perusahaan
+            </p>
+        </div>
+        <div class="col-12 px-5 item-center height-50 wight-50">
+            <!-- <img src="/assets/images/solution.png" class="img-fluid"> -->
+
+            <!-- <div class="max-w-2xl mx-auto">
+                <div id="default-carousel" class="relative" data-carousel="static">
+                    <div class="overflow-hidden relative h-56 rounded-lg sm:h-64 xl:h-80 2xl:h-96">
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <span
+                                class="absolute top-1/2 left-1/2 text-2xl font-semibold text-white -translate-x-1/2 -translate-y-1/2 sm:text-3xl ">Primer
+                                Slide</span>
+                            <img src="https://flowbite.com/docs/images/carousel/carousel-1.svg"
+                                class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                                alt="...">
+                        </div>
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <img src="https://flowbite.com/docs/images/carousel/carousel-2.svg"
+                                class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                                alt="...">
+                        </div>
+                        <div class="hidden duration-700 ease-in-out" data-carousel-item>
+                            <img src="https://flowbite.com/docs/images/carousel/carousel-3.svg"
+                                class="block absolute top-1/2 left-1/2 w-full -translate-x-1/2 -translate-y-1/2"
+                                alt="...">
+                        </div>
+                    </div>
+                    <div class="flex absolute bottom-5 left-1/2 z-30 space-x-3 -translate-x-1/2">
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 1"
+                            data-carousel-slide-to="0"></button>
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 2"
+                            data-carousel-slide-to="1"></button>
+                        <button type="button" class="w-3 h-3 rounded-full" aria-current="false" aria-label="Slide 3"
+                            data-carousel-slide-to="2"></button>
+                    </div>
+                    <button type="button"
+                        class="flex absolute top-0 left-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+                        data-carousel-prev>
+                        <span
+                            class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30  group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15 19l-7-7 7-7"></path>
+                            </svg>
+                            <span class="hidden">Anterior</span>
+                        </span>
+                    </button>
+                    <button type="button"
+                        class="flex absolute top-0 right-0 z-30 justify-center items-center px-4 h-full cursor-pointer group focus:outline-none"
+                        data-carousel-next>
+                        <span
+                            class="inline-flex justify-center items-center w-8 h-8 rounded-full sm:w-10 sm:h-10 bg-white/30 0 group-focus:ring-4 group-focus:ring-white  group-focus:outline-none">
+                            <svg class="w-5 h-5 text-white sm:w-6 sm:h-6" fill="none" stroke="currentColor"
+                                viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M9 5l7 7-7 7">
+                                </path>
+                            </svg>
+                            <span class="hidden">Siguiente</span>
+                        </span>
+                    </button>
+                </div>
+                <p class="mt-5">Este slide carousel se ha hecho con Tailwindcss
+                </p>
+                <script src="https://unpkg.com/flowbite@1.4.0/dist/flowbite.js"></script>
+            </div> -->
+            <div class="container-sm w-500 h-500">
+                <div id="carouselExampleCaptions" class="carousel slide" data-ride="carousel">
+                    <ol class="carousel-indicators">
+                        <li data-target="#carouselExampleCaptions" data-slide-to="0" class="active"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="1"></li>
+                        <li data-target="#carouselExampleCaptions" data-slide-to="2"></li>
+                    </ol>
+                    <div class="carousel-inner">
+                        <div class="carousel-item active item-center">
+                            <img src="/assets/images/1.png" class="d-block w-50 mx-auto" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>First slide label</h5>
+                                <p>Some representative placeholder content for the first slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/assets/images/2.png" class="d-block w-50 mx-auto" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Second slide label</h5>
+                                <p>Some representative placeholder content for the second slide.</p>
+                            </div>
+                        </div>
+                        <div class="carousel-item">
+                            <img src="/assets/images/3.png" class="d-block w-50 mx-auto" alt="...">
+                            <div class="carousel-caption d-none d-md-block">
+                                <h5>Third slide label</h5>
+                                <p>Some representative placeholder content for the third slide.</p>
+                            </div>
+                        </div>
+                    </div>
+                    <button class="carousel-control-prev" type="button" data-target="#carouselExampleCaptions"
+                        data-slide="prev">
+                        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </button>
+                    <button class="carousel-control-next" type="button" data-target="#carouselExampleCaptions"
+                        data-slide="next">
+                        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </button>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
